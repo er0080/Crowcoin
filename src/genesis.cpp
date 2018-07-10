ab1d109f75a52f8b1f2d3a5e84bfb0a0e9244709
er0080 <eric@ericandbrianne.com>
2018-07-10 03:09:29 +0000

    tweaked genesis utility

--- src/genesis.cpp
+++ src/genesis.cpp
@@ -6,31 +6,24 @@
 
 #include "chainparams.cpp"
 #include <sstream>
+#include "pow.h"
+#include <consensus/consensus.h>
+#include <consensus/params.h>
+#include <consensus/validation.h>
 
 int main(int argc, char* argv[]) {
 
   uint32_t nBits = 0x1e00ffff;
-/*  int i;
-
-  for (i=0; i < argc; i++){
-    printf("%s\n", argv[i]); 
-  }
-
-  if (argc > 0) {  
-    std::istringstream ss( argv[0] );
-    
-    if (ss >> diff) {
-      printf("Invalid difficulty.\n");
-      return 1;
-    }
-  }
-*/
   CBlock genesis = CreateGenesisBlock(1530974244, 0, nBits, 4, 5000 * COIN);
+  
+  Consensus::Params consensus;
+  consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); 
 
   printf("Hashing genesis block: CreateGenesisBlock(1530974244, nonce, %#010x, 4, 5000 * COIN)\n", nBits);
-
-  for(genesis.nNonce = 0; genesis.GetHash() < uint256S("0x000000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); genesis.nNonce++)
+  
+  while (!CheckProofOfWork(genesis.GetHash(), nBits, consensus))
   {
+    genesis.nNonce++;
   }
   
   printf("Genesis merkle root: %s\n", genesis.hashMerkleRoot.ToString().c_str());
